7926acab89b0aace4f9b0210b4cf8efdfb51ccea
Mathias Meyer <meyer@paperplanes.de>
2011-06-23 09:43:21 +0200

    Bump package version to 1.2.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def make_pb():
 if __name__ == "__main__":
     setup(
         name='riak',
-        version='1.2.1',
+        version='1.2.2',
         packages = find_packages(),
         install_requires = ['protobuf>=2.3.0'],
         dependency_links = ["http://downloads.basho.com/support"],
