c18c22d89bbb5a3dab0d414bfeb0e8f68cbfab9d
Russell Brown <russell.brown@mac.com>
2011-09-09 13:01:33 +0100

    Fix syntax error from 303bad9

--- riak/transports/pbc.py
+++ riak/transports/pbc.py
@@ -368,7 +368,7 @@ class RiakPbcTransport(RiakTransport):
         sent_len = self._sock.send(pkt)
         if sent_len != len(pkt):
             raise RiakError("PB socket returned short write %d - expected %d"%\
-                            (sent_len, len(pkt))
+                            (sent_len, len(pkt)))
 
     def recv_msg(self):
         self.recv_pkt()
