621df411d9ea1b1139f2042d25f22cab91d63744
Sean Cribbs <sean@basho.com>
2014-03-07 17:39:05 -0600

    See if we can tweak the path for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,13 @@ import sys, os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:
     import sphinx_bootstrap_theme
-from version import get_version
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+from version import get_version
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
