08844827dd5b77020ecd4df314608e5aa73082f7
Sean Cribbs <sean@basho.com>
2014-03-27 11:10:29 -0500

    Nit: move key filter tests to separate file.

---
+++ riak/tests/test_filters.py
@@ -0,0 +1,67 @@
+import platform
+from riak.mapreduce import RiakKeyFilter
+from riak import key_filter
+
+if platform.python_version() < '2.7':
+    unittest = __import__('unittest2')
+else:
+    import unittest
+
+
+class FilterTests(unittest.TestCase):
+    def test_simple(self):
+        f1 = RiakKeyFilter("tokenize", "-", 1)
+        self.assertEqual(f1._filters, [["tokenize", "-", 1]])
+
+    def test_add(self):
+        f1 = RiakKeyFilter("tokenize", "-", 1)
+        f2 = RiakKeyFilter("eq", "2005")
+        f3 = f1 + f2
+        self.assertEqual(list(f3), [["tokenize", "-", 1], ["eq", "2005"]])
+
+    def test_and(self):
+        f1 = RiakKeyFilter("starts_with", "2005-")
+        f2 = RiakKeyFilter("ends_with", "-01")
+        f3 = f1 & f2
+        self.assertEqual(list(f3),
+                         [["and",
+                           [["starts_with", "2005-"]],
+                           [["ends_with", "-01"]]]])
+
+    def test_multi_and(self):
+        f1 = RiakKeyFilter("starts_with", "2005-")
+        f2 = RiakKeyFilter("ends_with", "-01")
+        f3 = RiakKeyFilter("matches", "-11-")
+        f4 = f1 & f2 & f3
+        self.assertEqual(list(f4), [["and",
+                                     [["starts_with", "2005-"]],
+                                     [["ends_with", "-01"]],
+                                     [["matches", "-11-"]],
+                                     ]])
+
+    def test_or(self):
+        f1 = RiakKeyFilter("starts_with", "2005-")
+        f2 = RiakKeyFilter("ends_with", "-01")
+        f3 = f1 | f2
+        self.assertEqual(list(f3), [["or", [["starts_with", "2005-"]],
+                                     [["ends_with", "-01"]]]])
+
+    def test_multi_or(self):
+        f1 = RiakKeyFilter("starts_with", "2005-")
+        f2 = RiakKeyFilter("ends_with", "-01")
+        f3 = RiakKeyFilter("matches", "-11-")
+        f4 = f1 | f2 | f3
+        self.assertEqual(list(f4), [["or",
+                                     [["starts_with", "2005-"]],
+                                     [["ends_with", "-01"]],
+                                     [["matches", "-11-"]],
+                                     ]])
+
+    def test_chaining(self):
+        f1 = key_filter.tokenize("-", 1).eq("2005")
+        f2 = key_filter.tokenize("-", 2).eq("05")
+        f3 = f1 & f2
+        self.assertEqual(list(f3), [["and",
+                                     [["tokenize", "-", 1], ["eq", "2005"]],
+                                     [["tokenize", "-", 2], ["eq", "05"]]
+                                     ]])
