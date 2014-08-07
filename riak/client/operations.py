9d2759d1fbdb7aaa2bfbe92388ca44060a43b564
Sean Cribbs <sean@basho.com>
2014-08-07 09:46:23 -0500

    Document the requirement to close the iterator

--- riak/client/operations.py
+++ riak/client/operations.py
@@ -70,12 +70,33 @@ class RiakClientOperations(RiakClientTransport):
         .. warning:: Do not use this in production, as it requires
            traversing through all keys stored in a cluster.
 
+        The caller should explicitly close the returned iterator,
+        either using :func:`contextlib.closing` or calling `close`
+        explicitly. Consuming the entire iterator will also close the
+        stream. If it does not, the associated connection might not be
+        returned to the pool. Example::
+
+            from contextlib import closing
+
+            # Using contextlib.closing
+            with closing(client.stream_buckets()) as buckets:
+                for bucket_list in buckets:
+                    do_something(bucket_list)
+
+            # Explicit close()
+            stream = client.stream_buckets()
+            for bucket_list in stream:
+                 do_something(bucket_list)
+
+            stream.close()
+
         :param bucket_type: the optional containing bucket type
         :type bucket_type: :class:`~riak.bucket.BucketType`
         :param timeout: a timeout value in milliseconds
         :type timeout: int
         :rtype: iterator that yields lists of :class:`RiakBucket
              <riak.bucket.RiakBucket>` instances
+
         """
         _validate_timeout(timeout)
         if bucket_type:
@@ -168,6 +189,26 @@ class RiakClientOperations(RiakClientTransport):
         Queries a secondary index, streaming matching keys through an
         iterator.
 
+        The caller should explicitly close the returned iterator,
+        either using :func:`contextlib.closing` or calling `close`
+        explicitly. Consuming the entire iterator will also close the
+        stream. If it does not, the associated connection might not be
+        returned to the pool. Example::
+
+            from contextlib import closing
+
+            # Using contextlib.closing
+            with closing(client.stream_index(mybucket, 'name_bin', 'Smith')) as index:
+                for key in index:
+                    do_something(key)
+
+            # Explicit close()
+            stream = client.stream_index(mybucket, 'name_bin', 'Smith')
+            for key in stream:
+                 do_something(key)
+
+            stream.close()
+
         :param bucket: the bucket whose index will be queried
         :type bucket: RiakBucket
         :param index: the index to query
@@ -188,6 +229,7 @@ class RiakClientOperations(RiakClientTransport):
         :param term_regex: a regular expression used to filter index terms
         :type term_regex: string
         :rtype: :class:`riak.client.index_page.IndexPage`
+
         """
         if timeout != 'infinity':
             _validate_timeout(timeout)
@@ -314,6 +356,26 @@ class RiakClientOperations(RiakClientTransport):
 
         .. warning:: Do not use this in production, as it requires
            traversing through all keys stored in a cluster.
+
+        The caller should explicitly close the returned iterator,
+        either using :func:`contextlib.closing` or calling `close`
+        explicitly. Consuming the entire iterator will also close the
+        stream. If it does not, the associated connection might
+        not be returned to the pool. Example::
+
+            from contextlib import closing
+
+            # Using contextlib.closing
+            with closing(client.stream_keys(mybucket)) as keys:
+                for key_list in keys:
+                    do_something(key_list)
+
+            # Explicit close()
+            stream = client.stream_keys(mybucket)
+            for key_list in stream:
+                 do_something(key_list)
+
+            stream.close()
 
         :param bucket: the bucket whose properties will be set
         :type bucket: RiakBucket
@@ -460,6 +522,26 @@ class RiakClientOperations(RiakClientTransport):
         """
         Streams a MapReduce query as (phase, data) pairs. This is a
         generator method which should be iterated over.
+
+        The caller should explicitly close the returned iterator,
+        either using :func:`contextlib.closing` or calling `close`
+        explicitly. Consuming the entire iterator will also close the
+        stream. If it does not, the associated connection might
+        not be returned to the pool. Example::
+
+            from contextlib import closing
+
+            # Using contextlib.closing
+            with closing(mymapred.stream()) as results:
+                for phase, result in results:
+                    do_something(phase, result)
+
+            # Explicit close()
+            stream = mymapred.stream()
+            for phase, result in stream:
+                 do_something(phase, result)
+
+            stream.close()
 
         :param inputs: the input list/structure
         :type inputs: list, dict
