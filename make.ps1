bb21de9ff6bb61995383570528cacaa8ac60f36a
Luke Bakken <luke@bakken.io>
2016-04-27 07:13:39 -0700

    Add basic make.ps1 for Windows

---
+++ make.ps1
@@ -0,0 +1,20 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$env:RIAK_TEST_HOST = 'riak-test'
+$env:RIAK_TEST_PROTOCOL = 'pbc'
+$env:RIAK_TEST_PB_PORT = 10017
+$env:RUN_DATATYPES = 1
+$env:RUN_INDEXES = 1
+$env:RUN_POOL = 1
+$env:RUN_YZ = 1
+
+flake8 --exclude=riak/pb riak commands.py setup.py version.py
+if ($LastExitCode -ne 0) {
+    throw 'flake8 failed!'
+}
+
+python setup.py test
+if ($LastExitCode -ne 0) {
+    throw 'python tests failed!'
+}
